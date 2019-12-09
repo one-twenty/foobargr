94330e483ea58955a6c36db71b121ddaf1fda7fb
Nick Siakas <nikos.siakas@gmail.com>
2019-12-09 14:58:09 +0200

    Renamed project

--- programatistisgr/wsgi.py
+++ programatistisgr/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myforum.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programatistisgr.settings')
 
 application = get_wsgi_application()
