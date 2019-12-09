31d84c4065dce650f793934a9d21f759ad0ac74b
Nick Siakas <nikos.siakas@gmail.com>
2019-12-09 12:32:22 +0200

    Added register, login, logout

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 app_name = 'forum'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpRequest
+# from .form import RegisterForm
+#from django.contrib import messages
+
 
 def homepage(request):
     return render(request, 'forum/index.html')
 
+
+def register(request: HttpRequest):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect('forum:homepage')
+        # else:
+        #     for msg in form.error_messages:
+        #         print(form.error_messages[msg])
+        #     return render(request=request,
+        #                   template_name="forum/register.html",
+        #                   context={"form": form})
+    
+    form = UserCreationForm
+    return render(request, 'forum/register.html', {'form': form})
+
+
+def logout_request(request: HttpRequest):
+    logout(request)
+    #messages TODO
+    return redirect('forum:homepage')
+
+def login_request(request: HttpRequest):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    form = AuthenticationForm()
+    return render(request, 'forum/login.html', {'form': form})
+                
