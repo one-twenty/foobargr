c18301ea546f34bd79c19f0974e4ab228058885b
Nick Siakas <nikos.siakas@gmail.com>
2019-12-11 03:36:10 +0200

    Removed table name from fields

--- forum/models.py
+++ forum/models.py
@@ -3,20 +3,20 @@ from django.conf import settings
 from datetime import datetime    
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     parent_category = models.ForeignKey('self', null=True, blank=True, default=None, on_delete=models.SET_DEFAULT)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.category_name
+        return self.name
 
 
 class Topic(models.Model):
-    topic_title = models.CharField(max_length=200) 
-    topic_content = models.TextField()
-    topic_category = models.ForeignKey(Category, verbose_name='Category', default=0, on_delete=models.SET_DEFAULT)
+    title = models.CharField(max_length=200) 
+    content = models.TextField()
+    category = models.ForeignKey(Category, verbose_name='Category', default=0, on_delete=models.SET_DEFAULT)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=0, on_delete=models.SET_DEFAULT)
     datetime = models.DateTimeField(auto_now_add=True)
     
@@ -24,11 +24,11 @@ class Topic(models.Model):
         verbose_name_plural = 'Topics'
 
     def __str__(self):  
-        return self.topic_title
+        return self.title
 
 
 class Post(models.Model):
-    post_content = models.TextField()
+    content = models.TextField()
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=0, on_delete=models.SET_DEFAULT)
     datetime = models.DateTimeField(auto_now_add=True)
@@ -41,7 +41,7 @@ class Post(models.Model):
 
 
 class Reply(models.Model):
-    reply_content = models.TextField()
+    content = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=0, on_delete=models.SET_DEFAULT)
     datetime = models.DateTimeField(auto_now_add=True)
